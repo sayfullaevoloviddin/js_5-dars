8243a2d6c0a7854703109afe24758c6d56c89c30
sayfullaevoloviddin <sayfullaevoloviddin73@gmail.com>
2023-05-17 22:33:20 +0500

    js 5-dars yuklandi
    js 6-darsning kodlari

---
+++ js/main.js
@@ -0,0 +1,39 @@
+//let headTag = document.head;
+//let bodyTag = document.body;
+//let havola = document.documentURI;
+//let formalar = document.forms;
+//console.log(formalar);
+/*function meniBos(){
+    alert("Siz tugmani bosdingiz!");
+}*/
+/*function meniIkkiMartaBos(){
+    alert("Siz tugmani 2 marta bosdingiz!");
+}*/
+/*function tanlash(event) {
+    alert("Siz " + event.target.value + " moshinasini tanladingiz!");
+}*/
+/*function inputgabosish(){
+
+    console.log("Inputga bosildi!");
+}*/
+/*function inputdanchiqish(){
+    console.log("Inputdan chiqildi!")
+}*/
+/*function yozish(event){
+    console.log(event.target.value)
+}*/
+/*document.querySelector(".text").addEventListener("copy", function(event){
+    console.log("DIQQAT! Sizdan " + event.target.value + " nomli fayl ko'chirildi");
+})*/
+/*document.querySelector(".text").addEventListener("paste", function(event){
+    console.log("DIQQAT! Sizdan " + event.target.value + " nomli fayli Qo'shildi");
+})*/
+function harakatlanish(event){
+    console.log(event.clientY,event.clientX);
+}
+function kirish(){
+    alert("Hududga kirildi!");
+}
+function chiqish(){
+    alert("Hudud tark etildi!")
+}
